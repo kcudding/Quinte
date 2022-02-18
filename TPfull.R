d882feace2bec95e73153f8d191191a295872d3d
kcudding <kcudding@gmail.com>
2022-02-18 16:25:30 -0500

    9panel fig

--- TPfull.R
+++ TPfull.R
@@ -29,8 +29,8 @@ mmetric="Chla" #select which metric to analyze
 mlab=expression(Chlorophyll~a~(mu~g~L^-1))
 #mlab=expression(Total~phosophorus~(mg~L^-1))
 
-mlab=expression(Light~attenuation~(m^-1)) 
-#mlab=expression(Chlorophyll~a~(mu~g~L^-1))
+#mlab=expression(Light~attenuation~(m^-1)) 
+mlab=expression(Chlorophyll~a~(mu~g~L^-1))
 #mlab=expression(Total~phosophorus~(mg~L^-1))
 #rlab=expression(Rate~of~change~(mg~L^-1~year^-1))
 rlab=expression(Rate~of~change~(m^-1~year^-1))
@@ -128,7 +128,7 @@ round(concurvity(fm, full=FALSE)$worst,2)
 
 draw(fm, residuals=TRUE, rug=FALSE)
 
-plot(fm, seWithMean=TRUE, shift=coef(fm)[1], rug=FALSE, 
+drt=plot(fm, seWithMean=TRUE, shift=coef(fm)[1], rug=FALSE, 
      shade=TRUE,residuals=TRUE, pch=16)
 #by.resids=TRUE)
 dort=draw(fm, rug=FALSE, residuals=TRUE)
@@ -142,12 +142,14 @@ pdat=cbind(pdat,fmfit)
 fmderiv=as.data.frame(derivatives(fm, interval="simultaneous"))
 fmderiv$smooth=as.factor(fmderiv$smooth)
 lfm=split(fmderiv, fmderiv$smooth)
-par(mfrow=c(3,1), mar=c(1,4,0,3), oma=c(5,5,2,2),
+par(mfrow=c(3,1), mar=c(1,4,0,3), oma=c(5,4,2,2),
     cex.axis=1, cex.lab=1.75, cex.main=1.2, cex.sub=1)
-for (i in 2:4) {
+dseq=c(1,3,2)
+for (i in seq_along(dseq)) {
   dat=as.data.frame(lfm[i])
   colnames(dat)=colnames(fmderiv)
-  plot(derivative~data, data=dat, ylim=c(-1.1,1.1), typ="l", lwd=2)
+  plot(derivative~data, data=dat, ylim=c(-1.1,1.1), 
+       typ="l", lwd=2)
   lines(lower~data, data=dat)
   lines(upper~data, data=dat)
   abline(h=0)
@@ -161,14 +163,15 @@ pdat$yrse=yearfit$se
 
 mscale=c(0,
          max(pdat$upper, na.rm=T)+sscal)
-par(mfcol=c(3,2), mar=c(0.5,3,1,1), oma=c(4,3,0,0),
+#####
+par(mfcol=c(3,3), mar=c(0.5,5,1,1), oma=c(4,1,0,0),
     cex.axis=1.2, cex.lab=1.75, cex.main=1.2, cex.sub=1)
 for (i in 1:3){
   
   datre=pdat[pdat$Station_Acronym==stvec[i],]
-  plot(metric~year, xaxt="n",pch=16, data=datre,col="dark grey",
+  plot(metric~year, xaxt="n",pch=16, data=datre,
        ann=FALSE, xlim=xyear,ylim=c(0,39),las=2)
-  lines(fitted~year, lwd=2,data=datre,col="purple")
+  lines(fitted~year, lwd=2,data=datre,col=1)
 # lines(upper~year, data=datre,col="purple")
 #  lines(lower~year, data=datre,col="purple")
   x=c(rev(datre$lower), datre$upper)
@@ -176,34 +179,86 @@ for (i in 1:3){
   polygon(y,x, col=adjustcolor("grey",alpha.f=0.3), border=NA )
   legend("topright",stlabel[i], bty="n",
          cex=1.5)
+  if ( i==3)axis(side=1, las=1)
 }
-mtext(mlab, side = 2, line = 0, outer = TRUE, at = NA,
+mtext(mlab, side = 2, line = -2, outer = TRUE, at = NA,
       adj = NA, padj = NA, cex = 1.4, col = NA, font = NA)
 mtext("year", side = 1, line = +2, outer = TRUE, at = NA,
       adj = NA, padj = NA, cex = 1.4, col = NA, font = NA)
 
 pout=partial_residuals(fm, select = "s(year)", partial_match=TRUE)
 sout=smooth_estimates(fm, smooth = "s(year)", partial_match=TRUE)
+pout=pout[,c(1,3,2)]
+# or selected smooths
+dfsm=edf(fm, smooth = colnames(pout))
+dfsm=as.data.frame(dfsm)
+drt[[2]]$p.resid
+
+colnames(pout)=c("s(year):B", "s(year):HB", "s(year):C")
+
+pdat=cbind(pdat,pout)
+pdat$mgB=drt[[2]]$p.resid
+pdat$mgHB=drt[[2]]$p.resid
+#par(mfcol=c(3,2), mar=c(0.5,3,1,1), oma=c(4,3,0,0),
+ #   cex.axis=1.2, cex.lab=1.75, cex.main=1.2, cex.sub=1)
+
+for (i in 1:3){
+  mgres=drt[[i+1]]$p.resid
+  wind=which(pdat$Station_Acronym==stvec[i])
+  mgres=mgres[wind]
+  datsm=sout[sout$Station_Acronym==stvec[i],]
+  datsres=pdat[wind,]
+  yname=(paste0("s(year):",stvec[i]))
+ 
+  plot(datsres[,yname]~year, xaxt="n",pch=16,col="grey", 
+       data=datsres,
+       ann=FALSE, xlim=xyear,ylim=c(-15,15),las=2)
+  lines(est~year, lwd=2,data=datsm,col="dark green")
+ # points(mgres~year, data=datsres, col="red")
+  datsm$upper = datsm$est + (dfsm[i,2] * datsm$se)
+  datsm$lower = datsm$est - (dfsm[i,2] * datsm$se)
+  datsm$upper =  datsm$est + 2*datsm$se
+  datsm$lower =  datsm$est - 2*datsm$se
+   lines(upper~year, data=datsm,col="dark green")
+    lines(lower~year, data=datsm,col="dark green")
+  x=c(rev(datsm$lower), datsm$upper)
+  y=c(rev(datsm$year), datsm$year)
+  polygon(y,x, col=adjustcolor("dark green",alpha.f=0.1), border=NA )
+  
+  if (i==2) mtext("Partial effect", side = 2, line = 3, outer = FALSE, at = NA,
+                adj = NA, padj = NA, cex = 1.4, col = NA, font = NA)
+  if ( i==3)axis(side=1, las=1)
+  #legend("topright",stlabel[i], bty="n",cex=1.5)
+}
 
 
-
-
-
-par(mfrow=c(3,1), mar=c(1,4,0,3), oma=c(5,5,2,2),
-    cex.axis=1, cex.lab=1.75, cex.main=1.2, cex.sub=1)
 derivatives(fm, interval="simultaneous")
 dtall=as.data.frame(derivatives(fm, interval="simultaneous"))
 dtall=as.data.frame(derivatives(fm, interval="simultaneous"))
 dtall$smooth=as.factor(dtall$smooth)
 dlit=split(dtall, dtall$smooth)
-for (i in 2:4) {
+yaxl=c(-1.5,1.5)
+dseq=c(2,4,3)
+for (j in seq_along(dseq)) {
+  i=dseq[j]
   dat=dlit[i]
   dat=as.data.frame(dlit[i])
   colnames(dat)=colnames(dtall)
-  plot(derivative~data, data=dat, ylim=c(-1,1), typ="l", lwd=2)
-  lines(lower~data, data=dat)
-  lines(upper~data, data=dat)
-  abline(h=0)
-  abline(v=1994)
+  plot(derivative~data, data=dat,xaxt="n",ann=FALSE,
+       ylim=yaxl, typ="l", lwd=2, col="dark red")
+  lines(lower~data, data=dat, col="dark red")
+  lines(upper~data, data=dat,col="dark red")
+  x=c(rev(dat$lower), dat$upper)
+  x[x>yaxl[2]]<-yaxl[2]+.1
+  x[x< yaxl[1]]<-yaxl[1]-.1
+  y=c(rev(dat$data), dat$data)
+  polygon(y,x, col=adjustcolor("dark red",alpha.f=0.1), border=NA )
+  
+  abline(h=0, col="grey", lwd=2)
+  abline(v=1994, col="grey", lwd=2)
+  if (i==2) text(1997, 1.45, "1994", cex=1.2)
+  if (i==3)axis(side=1, las=1)
+  if (i==4) mtext("Derivative of smooth", side = 2, line = 3, outer = FALSE, at = NA,
+                  adj = NA, padj = NA, cex = 1.4, col = NA, font = NA)
   
 }
